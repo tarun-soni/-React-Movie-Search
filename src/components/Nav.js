a54e316684f3b9653897a07bd1b59e855de2c1cb
tarun-soni <soni.tarun26@gmail.com>
2020-06-13 23:30:29 +0530

    Made add PWA

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import '../App.css'
-import logo from "../logo.svg"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 function Nav() {
   const navStyle = {
-    color: "white"
-  }
+    color: "white",
+  };
   return (
     <div className="App">
-      <nav >
+      <nav>
         <ul className="nav-links">
-         
-
-          <Link style={navStyle} to='/contact'>
+          <Link style={navStyle} to="/contact">
             <li>Search Movies</li>
           </Link>
-          <Link style={navStyle} to='/books'>
+          <Link style={navStyle} to="/books">
             <li>Search Books</li>
           </Link>
-
         </ul>
       </nav>
     </div>
--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,68 +1,68 @@
 import React, { useState } from "react";
-import './SearchMovies.css'
-import MovieCard from "./Moviecard";
+import "./SearchMovies.css";
 import BookCard from "./BookCard";
 
 function SearchBook() {
- //query hook 
- const [query, setQuery] = useState('')
+  //query hook
+  const [query, setQuery] = useState("");
 
- //total json
- const [fetchedData, setFetchedData] = useState({})
+  //total json
+  const [fetchedData, setFetchedData] = useState({});
 
- // @2.results array 
- const [books, setBooks] = useState([])
+  // @2.results array
+  const [books, setBooks] = useState([]);
 
- const searchBookFunc = async (e) => {
-   
-  e.preventDefault();
-  console.log("submitting");
-  // const API_KEY = process.env.REACT_APP_API_KEY
-  const url =  `https://www.googleapis.com/books/v1/volumes?q=${query}`
+  const searchBookFunc = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+    // const API_KEY = process.env.REACT_APP_API_KEY
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
 
-  try {
-      const response = await fetch(url)
-      const data = await response.json()
-      if(data!=null) 
-        setFetchedData(data)
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data != null) setFetchedData(data);
 
-      setBooks(data.items)
-  }
-  catch (error) {
-      console.err('error is', error);
-  }
-
-}
+      setBooks(data.items);
+    } catch (error) {
+      console.err("error is", error);
+    }
+  };
 
   return (
-      
-      <div>
-            <form className="search-form" onSubmit={searchBookFunc}>
-                <label className="label" htmlFor="query">Enter Book Name</label>
-                <input className="input-movie" type="text" name="query"
-                    placeholder="maybe Hamlet"
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)} />
-                <button className="search-button" type="submit">Search</button>
-            </form>
+    <div>
+      <form className="search-form" onSubmit={searchBookFunc}>
+        <label className="label" htmlFor="query">
+          Enter Book Name
+        </label>
+        <input
+          className="input-movie"
+          type="text"
+          name="query"
+          placeholder="maybe Hamlet"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
 
-            <h2>No of pages: {fetchedData.page}  of {fetchedData.total_pages}</h2>
+      <h2>
+        No of pages: {fetchedData.page} of {fetchedData.total_pages}
+      </h2>
 
-            <h2>Total No. of Resuts:{fetchedData.totalItems}</h2>
+      <h2>Total No. of Resuts:{fetchedData.totalItems}</h2>
 
-            <div className="card-list">
-                {
-                    books.filter(book => book.volumeInfo.imageLinks).map(book => (
-                         <BookCard book={book} key={book.id} />
-                    )
-                    )
-                }
-            </div>
-        </div>
+      <div className="card-list">
+        {books
+          .filter((book) => book.volumeInfo.imageLinks)
+          .map((book) => (
+            <BookCard book={book} key={book.id} />
+          ))}
+      </div>
+    </div>
   );
 }
 
 export default SearchBook;
-
-
-
